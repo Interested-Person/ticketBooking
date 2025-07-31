96ab37843b673cd8f94b05fd3c1fef5755843f3c
Interested-Person <110773785+Interested-Person@users.noreply.github.com>
2025-07-31 22:44:52 +0530

    now can see all events in admin, edited eventcard to remove read more

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
 import EventCard from "../components/EventCard"
 import AddEntry from "../components/Modal/addEntryAdmin"
+import { useEvent } from "../hooks/useEvent"
 const Admin = () => {
     const [openAddEntry, setOpenAddEntry] = useState(false);
+    const { ticketsFeed } = useEvent()
+    if (!ticketsFeed) return (<div className="text-white">Loading</div>)
     return (
-        <div className="text-white flex flex-col gap-5 py-5">
-            {/* <h1>Admin</h1> */}
-            <section className="flex items-center justify-center ">
-                <button className="bg-sky-500 p-2 rounded-md " onClick={()=>{setOpenAddEntry(true)}}>Add new entry</button>
-            </section>
-      
-        {openAddEntry && <AddEntry onClose={() => setOpenAddEntry(false)} />}
+        <div className="">
+
+            <div className="text-white flex flex-col gap-5 py-5">
+                {/* <h1>Admin</h1> */}
+                <section className="flex items-center justify-center ">
+                    <button className="bg-sky-500 p-2 rounded-md " onClick={() => { setOpenAddEntry(true) }}>Add new entry</button>
+                </section>
+
+                {openAddEntry && <AddEntry onClose={() => setOpenAddEntry(false)} />}
+            </div>
+            <div className="flex gap-5">
+                {ticketsFeed.map((event) => (
+                    <EventCard
+                        key={event.id}
+                        eventID={event.id}
+                        eventImg={event.imageURL}
+                        eventName={event.name}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
