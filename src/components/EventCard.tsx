2f6bb7c17297b245a759471cc7832460e607f4e9
Interested-Person <110773785+Interested-Person@users.noreply.github.com>
2025-07-31 12:59:59 +0530

    added placeholder image, added EventCard

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,21 +1,25 @@
-const EventCard = () => {
+
+import placeholder from '../assets/placeholder.png'
+import { useNavigate } from 'react-router-dom'
+
+const EventCard = ({ eventName, eventID, eventDescription, eventImg }: { eventName: string, eventID: string, eventDescription: string, eventImg: string }) => {
+    const navigate = useNavigate()
     return (
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-40 md:max-w-64 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                <img className="rounded-t-lg w-full" src={eventImg || placeholder} alt="" />
             </a>
-            <div className="p-5">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Noteworthy technology acquisitions 2021
+            <div className="p-2">
+                <p >
+                    <h5 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {eventName}
                     </h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in
-                    reverse chronological order.
                 </p>
-                <a
-                    href="#"
+                <p className="mb-2 font-normal text-gray-700 dark:text-gray-400">
+                    {eventDescription}
+                </p>
+                <button
+                    onClick={() => { navigate("/event/" + eventID) }}
                     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                     Read more
@@ -34,7 +38,7 @@ const EventCard = () => {
                             d="M1 5h12m0 0L9 1m4 4L9 9"
                         />
                     </svg>
-                </a>
+                </button>
             </div>
         </div>
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-            <EventCard />
+            <EventCard eventID='1' eventDescription='great event must come' eventName='event1' eventImg='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Example_image.svg/600px-Example_image.svg.png' />
         </div>
     )
 }
