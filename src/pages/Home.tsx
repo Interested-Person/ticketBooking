bca20ad128ea00cbaa5ef4b64988b5e02182aeb9
Pritam2707 <priyamgamer0@gmail.com>
2025-08-01 00:51:10 +0530

    Added search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import ChatIcon from '../assets/chat.svg'
 import { useNavigate } from 'react-router-dom';
 const Home = () => {
     const Navigate = useNavigate();
-    const { ticketsFeed, loading } = useEvent();
+    const { ticketsFeed, loading,isSearching,searchFeed } = useEvent();
     if (loading) {
         return <LoadingSpinner />
     }
@@ -13,13 +13,11 @@ const Home = () => {
         <div>
             <h1 className='text-white text-xl p-4'>Available Events</h1>
             <div className='flex p-4 basis-1/2 flex-wrap md:basis-1/3 xl:basis-1/4 gap-5'>
-                {ticketsFeed.map((event) => (
-                    <EventCard
-                        key={event.id}
-                        event={event}
-                    />
-                ))}
-            </div>
+    {(isSearching ? searchFeed : ticketsFeed).map(event => (
+        <EventCard key={event.id} event={event} />
+    ))}
+</div>
+
             <img onClick={() => Navigate("/chat")} src={ChatIcon} className='absolute bottom-4 right-4 w-12 h-12' />
 
         </div>
