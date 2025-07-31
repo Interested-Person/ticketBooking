b57c1fa074d479aba9b229b39ef5db491ce404cf
Interested-Person <110773785+Interested-Person@users.noreply.github.com>
2025-07-31 12:41:37 +0530

    nothing much

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Admin from './pages/Admin'
+import SignIn from './pages/SignIn'
 function App() {
 
   return (
     <>
       <Router>
         <Routes>
+
+          <Route path="/admin" element={<Admin />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
           <Route path="/*" element={<Home />} ></Route>
+
+
+
         </Routes>
       </Router>
     </>
