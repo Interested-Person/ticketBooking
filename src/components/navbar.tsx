b29eb74e9c431d287be5d263eeefa3d6c6e20729
Interested-Person <110773785+Interested-Person@users.noreply.github.com>
2025-07-31 20:17:38 +0530

    Removed tags, added useUser(),  added Accounts page, displayed events in home page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,10 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useAuth } from "../hooks/useAuth";
 import PromptMore from "./Modal/promptMore";
 import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+    const navigate = useNavigate()
     const handleLogin = () => {
         const provider = new GoogleAuthProvider();
         signInWithPopup(auth, provider);
@@ -12,11 +14,11 @@ export default function Navbar() {
     return (<>
         <nav className="bg-sky-950 w-screen items-center px-4 text-white h-16 flex justify-between">
             <div className="flex items-center gap-2">
-                <img src="/logo.svg" className="h-16" alt="" />
+                <img onClick={() => navigate("/")} src="/logo.svg" className="h-16" alt="" />
             </div>
             {isLoggedIn ? (<div>
 
-                <img referrerPolicy="no-referrer" className="w-10 rounded-full" loading="lazy" src={user?.pfpUrl} alt="" />
+                <img onClick={() => navigate("/account")} referrerPolicy="no-referrer" className="w-10 rounded-full" loading="lazy" src={user?.pfpUrl} alt="" />
 
             </div>) : (
 
