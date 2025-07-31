91edd37c8b7396d41bd107f5bf89cda886354ec5
Pritam2707 <priyamgamer0@gmail.com>
2025-07-31 18:24:38 +0530

    updaetd hooks final

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -7,7 +7,7 @@ const Admin = () => {
         <div className="text-white flex flex-col gap-5 py-5">
             {/* <h1>Admin</h1> */}
             <section className="flex items-center justify-center ">
-                <button className="bg-sky-500 p-2 rounded-md ">Add new entry</button>
+                <button className="bg-sky-500 p-2 rounded-md " onClick={()=>{setOpenAddEntry(true)}}>Add new entry</button>
             </section>
         <section className="flex flex-col items-center justify-center gap-4">
             <h2>Currently hosted Tickets</h2>
